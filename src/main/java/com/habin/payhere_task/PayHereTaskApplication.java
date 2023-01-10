2a230a3bcb48a3bc9f96f533dd9dc949d9d0615a
habin <danny9643@naver.com>
2023-01-10 19:49:56 +0900

    first commit

---
+++ src/main/java/com/habin/payhere_task/PayHereTaskApplication.java
@@ -0,0 +1,13 @@
+package com.habin.payhere_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayHereTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayHereTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/habin/payhere_task/PayHereTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habin.payhere_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayHereTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
